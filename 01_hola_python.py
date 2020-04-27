32b177efb630e06539c68b132fa6736cab6e5f5b
Felipe Contreras <hobber.mallow@gmail.com>
2020-04-27 13:04:31 -0500

    initial

---
+++ 01_hola_python.py
@@ -0,0 +1,26 @@
+
+print("Hola Python!")
+
+print(2*3)
+
+print(2*7)
+
+print("Hola",
+      "adios",
+      "aja")
+
+print("""Hola
+adios
+aja""")
+
+print('''Mexicanos
+al grito
+de guerra''')
+
+print("""sapito""")
+
+print('Las cadenas con comillas funcionan "igual" que con apóstrofes')
+
+print("aquí no pasa \"nada\"")
+
+print("""Pues "aquí" 'tampoco'...""")
---
+++ 03_pide_cantidad_concepto_y_precio.py
@@ -0,0 +1,48 @@
+
+"""
+Ejemplos de entrada y salida.
+(esta es la cadena de documentación)
+"""
+
+"""
+Ejemplo 1.
+
+cantidad = 1
+print("cantidad =", cantidad)
+"""
+
+"""
+Ejemplo 2.
+
+cantidad = input("Dame la cantidad:")
+print("cantidad =", cantidad)
+"""
+
+"""Ejemplo 3.
+cantidad = input("Dame la cantidad:")
+concepto = input("Dame el concepto:")
+precio = input("Dame el precio:")
+print("cantidad =", cantidad)
+print("concepto =", concepto)
+print("precio =", precio)"""
+
+
+"""Ejemplo 4.
+cantidad = input("Dame la cantidad:")
+concepto = input("Dame el concepto:")
+precio = input("Dame el precio:")
+print("|------------|----------------|-------|----------|")
+print("|  Cantidad  |    Concepto    |Precio |  Precio  |")
+print("|------------|----------------|-------|----------|")
+print(cantidad, concepto, precio)"""
+
+"""Ejemplo 5."""
+cantidad = input("Dame la cantidad:")
+concepto = input("Dame el concepto:")
+precio = input("Dame el precio:")
+print("|------------|----------------|-------|----------|")
+print("|  Cantidad  |    Concepto    |Precio |Precio tot|")
+print("|------------|----------------|-------|----------|")
+print("|", cantidad, "|", concepto, "|", precio, "|")
+
+
